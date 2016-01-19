7bdca971cbd9fb910e9348a4262f9468aa0a03d5
Andrzej Jakuszyk <ajakuszyk@gmail.com>
2016-01-19 17:10:24 +0000

    Example unit tests added

---
+++ DogTrainingWindowsService.Tests.Unit/DogTrainingWindowsServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace DogTrainingWindowsService.Tests.Unit
+{
+    [TestFixture]
+    public class DogTrainingWindowsServiceTests
+    {
+        [Test]
+        public void FirstUnitTestExample()
+        {
+            Assert.AreEqual(2 + 2, 4);
+        }
+
+        [Test]
+        public void SecondUnitTestExample()
+        {
+            Assert.Throws<DivideByZeroException>(delegate {
+                var x = 0;
+                var divideMe = 10 / x;
+            });
+        }
+
+        [Test]
+        public void ThirdUnitTestExample()
+        {
+            var model = new DogBarkModel();
+            Assert.NotNull(model);
+        }
+    }
+}
